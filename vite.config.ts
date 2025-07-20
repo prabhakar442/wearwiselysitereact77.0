f51848aadf29932055e1f7a2deae1f63f8a67eb1
prabhakar442 <prabhakarjd2@gmail.com>
2025-07-20 22:45:32 +0530

    new vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    target: 'esnext',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
